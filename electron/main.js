ac7082b68ac2945addd080f7b962e21dc33d29a7
Abhishek Kasireddy <akasireddy99@gmail.com>
2020-06-30 15:22:00 +0530

    added electron

---
+++ electron/main.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  if (process.env.NODE_ENV === "development") {
+    win.loadURL("http://localhost:3000/");
+  } else {
+    win.loadFile("./build/index.html");
+  }
+
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.on(
+  "certificate-error",
+  (event, webContents, url, error, cert, callback) => {
+    event.preventDefault();
+    callback(true);
+  }
+);
